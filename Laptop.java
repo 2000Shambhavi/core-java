95be5b8e8dc2e718626765753b62992b6d793e0d
Sham-6648 <107555373+Sham-6648@users.noreply.github.com>
2023-06-21 22:57:58 +0530

    test-push

---
+++ Laptop.java
@@ -0,0 +1,40 @@
+public class Laptop{
+	public static void main(String[] args){
+		add();
+		sub();
+		mul();
+		div();
+		mod();
+	}
+	public static void add(){
+		int[] totalNumberHorses={1,2,3,4,5};
+		for(int i=0;i<totalNumberHorses.length;i++){
+			System.out.println(totalNumberHorses[i]);
+			}
+		}
+	public static void sub(){
+		float[] cgpaOfStudents={8.1f,8.2f,8.3f,8.4f,8.5f};
+		for(int i=0;i<cgpaOfStudents.length;i++){
+		System.out.println(cgpaOfStudents[i]);
+		}
+	}
+	public static void mul(){
+		int[] numberOfFloorsInBuilding={1,2,3,4,5};
+		for(int i=0;i<numberOfFloorsInBuilding.length;i++){
+		System.out.println(numberOfFloorsInBuilding[i]);
+		}
+	}
+	public static void div(){
+		int[] numberOfMonthsInYear={1,2,3,4,5};
+		for(int i=0;i<numberOfMonthsInYear.length;i++){
+		System.out.println(numberOfMonthsInYear[i]);
+		}
+	}
+	public static void mod(){
+		int[] numberOfStudentsInClass={1,2,3,4,5};
+		for(int i=0;i<numberOfStudentsInClass.length;i++){
+		System.out.println(numberOfStudentsInClass[i]);
+		}
+	}	
+}	
+		
