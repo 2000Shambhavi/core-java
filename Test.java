1d474b5bf349cb4be6c7283e7e4a1a56ac2ef1b7
Sham-6648 <107555373+Sham-6648@users.noreply.github.com>
2023-07-22 14:23:36 +0530

    test-push

---
+++ Test.java
@@ -0,0 +1,41 @@
+
+1. 31
+2. Zudio
+3.  50
+4.Sum of value is 200 and length is 4
+5.
+6. Method Parameters are the values specified inside the method braces. They are used to specify some particular value of a class into it.
+
+7.  Compile-time polymorphism is also known as static polymorphism or early binding.  Compile-time   polymorphism is a polymorphism that 	  is resolved during the compilation process.
+	It is achieved through method overloading.
+
+8. Var-args are the acronym of Variable Arguments, an argument accepts only the arbitrary values of integers inside the method braces in the parameter value.
+
+
+9. A package is a collection of set of classes and interfaces. That it saves in an organized way one after the other as we mentioned into it.
+	eg:-com.xworks.special.test
+	this organizes each individual floder as shown in the example above.
+	a package comprises of a reverse domain of particular url followed by .projectname.
+	
+	
+10. In Java  Constructor Chaining can be defied as the process calling one constructor from the another constructor with respect to an object.
+	It can be achieved either by this() or super() methods.
+	this() can be used to chain any object from the same class where as super() can be used to chain any object from the parent class.
+11. 
+12.
+13.
+
+14. An Enumeration also called as is enum is a type of datatype that enables variables to be a set of predefined constant values.
+	for eg, the direction method includes 4 parameters i.e, East, West, North and South. These are the set of constants that cannot be changed.
+	in simple, enum takes the universal truth values into it which are not changeable.
+	
+15. When an new object is created inside a method, the JVM first loads the main class to the class loader then it searches for the main   	method if the main method is found it creates a stack memory then if a new method is found inside the main method it creates a stack 	frame into the stack memory. The stack frame has method name variable and execution line no into it when an object is created inside 	this method an instance memory is created outside the stack memory and each object is stored into them. Instance memory is also known 	as Heap memory.
+
+	 The objects reference address is linked to that new object created inside the stack frame of that particular method in a stack memory.
+	
+	The objected stored in the instance memory can only be accessed by its unique address of the memory that is indicated to the stack frame from the instance memory.
+	
+16. The members of the class are: -
+		    1.Instance members: - Instance members are the variables and methods that are associated with a specific instance of a class. When you create an object of a class, each object has its own copy of the instance variables, and you can use the object's instance methods to manipulate those variables.
+			 
+			2. Static members: -Static members are variables and methods that are associated with the class itself, rather than with a specific instance of the class. Static variables and methods are shared by all instances of the class, and you can access them using the class name, rather than a specific object.
