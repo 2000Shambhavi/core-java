f7721b875772de0bb572d25698a4cae72769a44c
Sham-6648 <107555373+Sham-6648@users.noreply.github.com>
2023-08-04 13:52:03 +0530

    Encapsulation

---
+++ Encapsulation/src/com/xworkz/runner/DosaRunner.java
@@ -0,0 +1,17 @@
+package com.xworkz.runner;
+
+public class DosaRunner {
+
+public static void main(String[] args) {
+	Dosa dosa = new Dosa();
+	System.out.println(dosa);
+	dosa.setPrice(60);
+	dosa.setType("Ghee dosa");
+	dosa.setIsCrispy(true);
+	dosa.setHotelName("Udupi hotel");
+	dosa.setColor("Brown");
+	
+	System.out.println(dosa);
+	
+	}
+}
---
+++ Encapsulation/src/com/xworkz/runner/TapRunner.java
@@ -0,0 +1,21 @@
+package com.xworkz.runner;
+
+public class TapRunner {
+	public class TapRunner {
+		
+		public static void main(String [] args) {
+			Tap tap = new Tap ();
+			System.out.println(tap);
+			
+			tap.setPrice(300);
+			tap.setLength("6m");
+			tap.setType("Bib Tap");
+			tap.setColor("Silver");
+			tap.setMadeOf("Brace");
+			
+			System.out.println(tap);
+			
+			}
+	}
+
+}
